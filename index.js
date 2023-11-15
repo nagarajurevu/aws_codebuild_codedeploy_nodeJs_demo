194a200f0fc01e07fc6294c7b3b279b065e42e4a
nagarajurevu <nagarajurevu@gmail.com>
2023-11-15 19:26:30 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World! from Nagaraju')
+  res.send('Hello World! from raju')
 })
 
 app.get('/status', (req, res) => {
